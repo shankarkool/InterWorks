5816a2bbe21e868fd82854a2b2905b00e59741fa
shankarkool <myfriend.shankar@gmail.com>
2020-12-20 15:49:44 +1100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,23 @@
+# InterWorks
+
+import shankar as he
+
+always:
+
+try {
+    his best and;
+    do {
+        the task perfectly;
+    }while (you still have a time);
+
+    if (he fails)
+        throw "all his mistakes";
+
+}catch (him){
+    he learn from it and
+    goto always;
+}
+
+for (oppurtunity; comes; only once){
+    so grab him;
+}
